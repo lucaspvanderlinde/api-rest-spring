67761ef72d66c8c5b479d45f21204c203f47f581
lucaspvanderlinde <lucas.passos@clicheriablumenau.com.br>
2021-10-04 12:44:51 -0300

    Criando repository

--- forum/src/main/java/br/com/estudo/forum/controller/HelloController.java
+++ forum/src/main/java/br/com/estudo/forum/controller/HelloController.java
@@ -1,8 +1,15 @@
 package br.com.estudo.forum.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.com.estudo.forum.dtos.TopicoDto;
+import br.com.estudo.forum.model.Curso;
+import br.com.estudo.forum.model.Topico;
 
 @Controller
 public class HelloController {
@@ -12,5 +19,22 @@ public class HelloController {
 	public String hello() {
 		return "Hello World";
 	}
+	
+	
+	// Exemplo 
+//	@RequestMapping("/topicos")
+//	public List<TopicoDto> lista() {
+//		
+//		Topico topico = Topico.builder()
+//				.titulo("Duvida")
+//				.mensagem("Duvida com Spring")
+//				.curso(Curso.builder()
+//						.nome("Spring")
+//						.categoria("Programação")
+//						.build())
+//				.build();
+//		
+//		// Uitilizando Arrays.asList para retornar uma lista com varios topicos
+//		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
 
 }
--- forum/src/main/java/br/com/estudo/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/estudo/forum/controller/TopicosController.java
@@ -1,35 +1,25 @@
 package br.com.estudo.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.estudo.forum.dtos.TopicoDto;
-import br.com.estudo.forum.model.Curso;
 import br.com.estudo.forum.model.Topico;
+import br.com.estudo.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
 	
+	@Autowired
+	private TopicoRepository topicoRepositoty;
+	
 	@RequestMapping("/topicos")
 	public List<TopicoDto> lista() {
-		
-		Topico topico = Topico.builder()
-				.titulo("Duvida")
-				.mensagem("Duvida com Spring")
-				.curso(Curso.builder()
-						.nome("Spring")
-						.categoria("Programação")
-						.build())
-				.build();
-		
-		// Uitilizando Arrays.asList para retornar uma lista com varios topicos
-		return TopicoDto.converter(Arrays.asList(topico, topico, topico));
-		
-		
-		
+		List<Topico> topicos = topicoRepositoty.findAll();
+		return TopicoDto.converter(topicos);
 	}
 
 }
