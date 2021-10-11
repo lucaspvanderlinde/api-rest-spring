aa507d3af8e5457b940fdc776df4aa5a669375c6
lucaspvanderlinde <lucas.passos@clicheriablumenau.com.br>
2021-10-11 12:57:51 -0300

    API Rest buscar topico ID

--- forum/src/main/java/br/com/estudo/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/estudo/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,12 +31,16 @@ public class TopicosController {
 		List<TopicoDto> topicos = TopicoDto.converter(topicoService.buscarTodos());
 		return ResponseEntity.ok(topicos);
 	}
-	
 	@PostMapping
 	public ResponseEntity<Topico>cadastrar(@RequestBody @Valid Topico topico, UriComponentsBuilder uriBuilder) {
 		topicoService.cadastrar(topico);
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(topico);
 	}
-
+	@GetMapping("/{id}")
+	public ResponseEntity<Topico>buscarPorId(@PathVariable Long id) {
+		Topico topico = topicoService.buscarPorId(id);
+		System.out.println(topico);
+		return ResponseEntity.ok(topico);
+	}
 }
